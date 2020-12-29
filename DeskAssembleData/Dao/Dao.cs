2e388545003c742f97c30321a1f339a6100f7cf1
choibj <choibyoungjin85@gmail.com>
2020-12-29 12:33:21 +0900

    초기 커밋.

---
+++ DeskAssembleData/Dao/Dao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskAssembleData.Dao
+{
+    public class Dao
+    {
+        public static OrderDao Order { get; } = new OrderDao();
+        public static CountryDao Country { get; } = new CountryDao();
+        public static ExpenseDao Expense { get; } = new ExpenseDao();
+        public static ItemDao Item { get; } = new ItemDao();
+        public static MovementDao Movement { get; } = new MovementDao();
+        public static TeamDao Team { get; } = new TeamDao();
+        public static WareHouseDao WareHouse { get; } = new WareHouseDao();
+        public static ContractDao Contract { get; } = new ContractDao();
+    }
+}
---
+++ DeskAssembleData/EFLibrary/DbContextCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskAssembleData.EFLibrary
+{
+    public class DbContextCreator
+    {
+        private const string ConnectionString = "metadata=res://*/DeskAssemble.csdl|res://*/DeskAssemble.ssdl|res://*/DeskAssemble.msl;provider=System.Data.SqlClient;provider connection string=\"data source=10.10.31.22;initial catalog=DeskAssembly;user id=sa;password=3512;MultipleActiveResultSets=True;App=EntityFramework\"";
+        public static DeskAssemblyEntities Create()
+        {
+            DeskAssemblyEntities context = new DeskAssemblyEntities(ConnectionString);
+
+            context.Configuration.ProxyCreationEnabled = false;
+
+            return context;
+        }
+    }
+}
+namespace DeskAssembleData
+{
+    public partial class DeskAssemblyEntities
+    {
+        public DeskAssemblyEntities(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}
