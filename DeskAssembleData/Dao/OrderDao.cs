5d2f595c1aa5537d4445e3b7b6ab21fed748ace7
신형 최 <hemelic@naver.com>
2020-12-30 17:42:44 +0900

    병합후 문제 발생해결

--- DeskAssembleData/Dao/OrderDao.cs
+++ DeskAssembleData/Dao/OrderDao.cs
@@ -111,7 +111,34 @@ namespace DeskAssembleData
                         return Salemodels;
                     }
                 }
+        }
 
+        public List<MapChartModel> GetPurchasedCountryModels()
+        {
+            {
+                using (var context = DbContextCreator.Create())
+                {
+                    var countries = context.Countries.ToList();
+
+                    List<MapChartModel> models = new List<MapChartModel>();
+                    foreach (Country country in countries)
+                    {
+                        var query = from x in context.Orders
+                                    where x.Contract.CountryId == country.CountryId && x.IsSale == false
+                                    select x.Quantity;
+
+                        var list = query.ToList();
+                        MapChartModel model = new MapChartModel();
+                        model.Value = list.Sum();
+                        model.Latitude = country.Latitude;
+                        model.Longitude = country.Longitude;
+
+                        models.Add(model);
+                    }
+
+                    return models;
+                }
+            }
 
         }
 
